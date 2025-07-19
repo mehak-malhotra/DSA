27cff826b18116e644e7f38a3553e60f739f629a
Mehak Malhotra <mehakmalhotra0112@gmail.com>
2025-07-19 22:58:36 +0530

    Updated

--- Sorting/Merge_Sort.java
+++ Sorting/Merge_Sort.java
@@ -10,7 +10,6 @@ class Solution {
         if (low >= high) return;
 
         int mid = low + (high - low) / 2;
-
         mergeSort(arr, low, mid);
         mergeSort(arr, mid + 1, high);
         merge(arr, low, mid, high);
