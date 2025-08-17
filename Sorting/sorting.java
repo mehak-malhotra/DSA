0cac9211f0237920e89aefa768d1f807f1929096
Mehak Malhotra <mehakmalhotra0112@gmail.com>
2025-08-17 22:42:07 +0530

    sorting practice

--- Sorting/sorting.java
+++ Sorting/sorting.java
@@ -4,7 +4,19 @@ public class sorting {
         Solution s= new Solution();
 
         // bubble sort
-        s.bubbleSort(arr);
+        // s.bubbleSort(arr);
+
+        // selection sort
+        // s.selectionSort(arr);
+
+        // insertion sort
+        // s.insertionSort(arr);
+
+        // quick sort
+        // s.quickSort(arr, 0, arr.length -1);
+
+        // merge sort
+        s.mergeSort(arr, 0, arr.length - 1);
 
         // print array
         for(int i=0; i<arr.length; i++){
@@ -21,6 +33,7 @@ class Solution{
     }
 
     void bubbleSort(int[] arr){
+        // swap adjacents
         int n = arr.length;
         for(int i=0; i<n-1; i++){
             for(int j=0; j<n-i-1; j++){
@@ -29,5 +42,69 @@ class Solution{
                 }
             }
         }
+    }
+
+    void selectionSort(int[] arr){
+        // find the minimum and keep at front
+        int n= arr.length;
+        for(int i=0; i<n-1; i++){
+            int mini= i;
+            for(int j= i+1; j<n; j++){
+                if(arr[j] < arr[mini]){
+                    mini= j;
+                }
+            }
+            swap(arr, mini, i);
+        }
+    }
+
+    void insertionSort(int[] arr){
+        int n= arr.length;
+        for(int i=1; i<n; i++){
+            int temp = arr[i];
+            int j= i-1;
+            while(j >= 0 && arr[j] > temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+    }
+
+    int partitionEl(int[] arr, int low, int high){
+        int pivot= arr[high];
+        int i= low;
+        int j= high-1;
+        while(i <= j){
+            while(i<=j && arr[i] <= pivot){
+                i++;
+            }
+            while(j>= i && arr[j] > pivot){
+                j--;
+            }
+
+            if(i<j) swap(arr, i, j);
+        }
+
+        arr[high]= arr[i];
+        arr[i]= pivot;
+
+        return i;
+    }
+
+    // pivot last element
+    void quickSort(int[] arr, int low, int high){
+        if(low >= high) return;
+        int partition= partitionEl(arr, low, high);
+        quickSort(arr, partition+1, high);
+        quickSort(arr, low, partition-1);
+    }
+
+    void merge(){
+
+    }
+
+    void mergeSort(int[] arr, int low, int high){
+
     }
 }
