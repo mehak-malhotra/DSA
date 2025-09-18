31850052d6d96ff4565fd7d0c7b0879428e96e6c
Mehak Malhotra <mehakmalhotra0112@gmail.com>
2025-09-18 23:16:07 +0530

    stack implementation using queue

---
+++ Stack/225_implement_stack_using_queues.java
@@ -0,0 +1,36 @@
+class MyStack {
+    Queue <Integer> q= new ArrayDeque<>();
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        int n= q.size();
+        q.add(x);
+        for(int i=0; i<n; i++){
+            int y= q.remove();
+            q.add(y);
+        }
+    }
+    
+    public int pop() {
+        return q.remove();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
